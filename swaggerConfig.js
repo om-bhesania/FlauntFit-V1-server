e5b73b86347086e00236446c2771c6ff9cb455f7
Om Bhesania <bhesaniaom@gmail.com>
2025-01-09 22:53:44 +0530

    Fix Swagger configuration to correctly define server URLs and update route path casing

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -9,14 +9,12 @@ const options = {
       description: "API documentation for the application",
     },
     servers: [
-      {
-        url: "http://localhost:3012",
-        url: "https://flauntfit-v1-server.onrender.com/",
-        // Update with your server URL
-      },
+      { url: "http://localhost:3012" },
+      { url: "https://flauntfit-v1-server.onrender.com" },
+      // Update with your server URL
     ],
   },
-  apis: ["./routes/*.js"], // Path to your route files
+  apis: ["./Routes/*.js"], // Path to your route files
 };
 
 const swaggerSpecs = swaggerJsDoc(options);
